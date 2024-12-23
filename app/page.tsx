8ccf01afdafe0bd791e765ffae49e9612d86193d
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-12-23 18:22:43 +0200

    Sidebar

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-amber-100">
-      <h1 className="text-3xl text-brand-100 font-bold">store It - The only solution you need .</h1>
-    </div>
-  );
-}

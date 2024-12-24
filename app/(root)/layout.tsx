685ffef3166887a177127a9308aa2b7332ffdf90
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-12-25 01:25:36 +0200

    file upload functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,10 +14,16 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />
-        <Header userId={currentUser.$id} accountId={currentUser.accountId} />        <div className="main-content">{children}</div>
+        <Header
+          userId={currentUser.$id}
+          accountId={currentUser.accountId}
+        />{' '}
+        <div className="main-content">{children}</div>
       </section>
     </main>
   );
 };
 
 export default Layout;
+
+// 3:10

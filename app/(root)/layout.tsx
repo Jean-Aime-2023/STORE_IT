c12a3fea3591c60527f58840d17d5b67fba5cfd8
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-12-25 01:23:50 +0200

    File Upload functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,14 +8,13 @@ import React from 'react';
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
-  if(!currentUser) return redirect('/sign-in')
+  if (!currentUser) return redirect('/sign-in');
   return (
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
-        <Header />
-        <div className="main-content">{children}</div>
+        <MobileNavigation {...currentUser} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />        <div className="main-content">{children}</div>
       </section>
     </main>
   );
